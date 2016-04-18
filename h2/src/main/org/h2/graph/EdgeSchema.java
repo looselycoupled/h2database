04c892f1ed911caaa1d2f64232d6c793895fb53d
Allen Leis <allen.leis@gmail.com>
2016-04-17 22:39:44 -0400

    EdgeSchema now has db session object and can traverse joins to find the ending vertex (if only one is expected)

--- h2/src/main/org/h2/graph/EdgeSchema.java
+++ h2/src/main/org/h2/graph/EdgeSchema.java
@@ -7,9 +7,12 @@ package org.h2.graph;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.h2.engine.Session;
 import org.h2.graph.EdgeSchema;
 import org.h2.table.Table;
 import org.h2.table.Column;
+import org.h2.result.Row;
+import org.h2.index.Cursor;
 
 
 
@@ -22,6 +25,7 @@ import org.h2.table.Column;
  */
 public class EdgeSchema {
 
+    private Session session;
     private String label;
     private Boolean directed;
     private List<JoinSchema> joins = new ArrayList<JoinSchema>();
@@ -47,7 +51,8 @@ public class EdgeSchema {
      * Constructor accepts a reference to the underlying table and whether
      * the edge should be considered directed
      */
-    public EdgeSchema(Boolean directed, String label) {
+    public EdgeSchema(Session session, Boolean directed, String label) {
+        this.session = session;
         this.directed = directed;
         this.label = label;
     }
@@ -56,7 +61,8 @@ public class EdgeSchema {
      * Constructor accepts a reference to the underlying table and defaults
      * to the edge not being directed
      */
-    public EdgeSchema(String label) {
+    public EdgeSchema(Session session, String label) {
+        this.session = session;
         this.directed = false;
         this.label = label;
     }
@@ -86,6 +92,56 @@ public class EdgeSchema {
         joins.add(j);
     }
 
+
+    /**
+     * Public method to find the matching vertex at the end of an edge relationship.
+     *
+     * NOTE THAT THIS ASSUMES THE VERTEX HAS ONLY ONE EDGE OF THIS TYPE.
+     */
+    public Vertex getTargetVertex(Vertex sourceVertex) {
+        Boolean init = true;
+        int targetId = sourceVertex.getAttributes().get("ID").getInt();
+        Row row = null;
+
+        // loop through joined tables
+        for (JoinSchema j: joins) {
+            if (init) {
+                init = false;
+            } else {
+                targetId = row.getValue(getColumnPosition(j.sourceTable, j.sourceColumn)).getInt();
+            }
+
+            // loop through rows of joined tables looking for our target row
+            Cursor c = j.targetTable.getScanIndex(session).find(session, null, null);
+            while (c.next()) {
+                Row cursorRow = c.get();
+                if (cursorRow.getValue(getColumnPosition(j.targetTable, j.targetColumn)).getInt() == targetId) {
+                    // we found our join row
+                    row = cursorRow;
+                    break;
+                }
+            }
+        }
+
+        Table t = joins.get(joins.size() - 1).targetTable;
+        Vertex endVertex = new Vertex(row, t.getColumns());
+        return endVertex;
+    }
+
+    public int getColumnPosition(Table table, String name) {
+        return getColumnPosition(table, table.getColumn(name.toUpperCase()));
+    }
+
+    public int getColumnPosition(Table table, Column column) {
+        int counter = 0;
+        for (Column c: table.getColumns()) {
+            if (c == column) {
+                return counter;
+            }
+            counter += 1;
+        }
+        return -1;
+    }
 
     public Boolean isDirected() {
         return directed;
